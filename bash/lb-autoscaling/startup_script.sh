f76553d882e4a78d69adcc3fac60b584ca4eb7f7
Michael Seevogel <michael@michaelseevogel.de>
2017-07-12 12:33:05 +0200

    sync

--- bash/lb-autoscaling/startup_script.sh
+++ bash/lb-autoscaling/startup_script.sh
@@ -8,7 +8,7 @@
 git_repo=https://raw.githubusercontent.com/ms217/google-cloud-examples/master/bash/lb-autoscaling/
 
 yum update -y
-yum install -y nginx net-tools bind-utils nmap tcpdump curl wget lynx iftop atop ntp ntpdate ntp-doc pure-ftpd centos-release-gluster310.noarch mlocate jwhois telnet ftp htop siege
+yum install -y nginx net-tools bind-utils nmap tcpdump curl wget lynx iftop atop ntp ntpdate ntp-doc pure-ftpd centos-release-gluster310.noarch mlocate jwhois telnet ftp htop siege lsof strace rsync
 yum install -y glusterfs-server glusterfs-coreutils 
 updatedb
 
@@ -21,15 +21,15 @@ timedatectl set-timezone $timezone
 setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
 
-[ -d /etc/sysctl.d ] && wget $git_repo/90-custom_sysctl.conf -O /etc/sysctl.d/90-custom_sysctl.conf && sysctl -p
+[ -d /etc/sysctl.d ] && wget $git_repo/90-custom_sysctl.conf -O /etc/sysctl.d/90-custom_sysctl.conf && sysctl -p /etc/sysctl.d/90-custom_sysctl.conf
 [ -d /etc/nginx ] && wget $git_repo/nginx.conf -O /etc/nginx/nginx.conf
 [ -d /etc/nginx/conf.d ] && wget $git_repo/vhost.conf -O /etc/nginx/conf.d/vhost.conf
 
 
 management_ip=$(curl "http://metadata.google.internal/computeMetadata/v1/project/attributes/management_ip" -H "Metadata-Flavor: Google")
 vhost_name=$(curl "http://metadata.google.internal/computeMetadata/v1/project/attributes/vhost_name" -H "Metadata-Flavor: Google")
-sed -i "s#\<MANAGEMENT_IP\>#`$management_ip`#g" /etc/nginx/nginx.conf
-sed -i "s#\<VHOST_NAME\>#`$vhost_name`#g" /etc/nginx/conf.d/vhost.conf
+sed -i "s#<MANAGEMENT_IP>#`$management_ip`#g" /etc/nginx/nginx.conf
+sed -i "s#<VHOST_NAME>#`$vhost_name`#g" /etc/nginx/conf.d/vhost.conf
 
 
 systemctl enable glusterd
